fffddcf6c68e406922daf9987ba90fa3fe0d7c77
Ilya Kirillov <kirillov.is83@gmail.com>
2022-08-04 11:07:34 +0500

    Paty3.2

---
+++ Paty3.2/Program.cs
@@ -0,0 +1,18 @@
+﻿//5. Напишите программу, которая на вход принимает одно число (N), а на выходе показывает все целые числа в промежутке от -N до N.
+
+Console.WriteLine("Введите первое число: ");
+int n = int.Parse(Console.ReadLine());
+int i = -1*n;
+
+while (i < 0)
+{
+    Console.WriteLine(i);
+    i = i + 1;
+}
+i = 0;
+
+while (i <= n)
+{
+    Console.WriteLine(i);
+    i = i +1;
+}
